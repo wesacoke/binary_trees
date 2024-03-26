334507e2fd87e29dbbafe69b38bbd6a722751c5c
wesacoke <wesaemmanuel02@gmail.com>
2024-03-26 02:57:49 -0700

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,11 +11,12 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    func(tree->n);
+	func(tree->n);
 
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
