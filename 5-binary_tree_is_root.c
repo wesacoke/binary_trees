e70fcab81bd2413f66fd5d400cc1d738fa9827d0
wesacoke <wesaemmanuel02@gmail.com>
2024-03-26 02:54:41 -0700

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,7 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
-
-    return (node->parent == NULL);
+	if (node == NULL)
+		return (0);
+	return (node->parent == NULL);
 }
